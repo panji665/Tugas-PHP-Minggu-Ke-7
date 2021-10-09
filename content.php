4dd50837c5af0c85969a371b787e58a9394a5b05
Panca Aji Nugraha <78069598+panji665@users.noreply.github.com>
2021-10-09 08:28:26 +0700

    Add files via upload

---
+++ content.php
@@ -0,0 +1,9 @@
+<?php 
+    $TITLE = "Blog Belajar PHP SMK - Coding Itu Mudah";
+    $LOGO = "Panji556";
+    $NAV = [
+        "Home" => "index.php",
+        "About" => "isi-php/about.php",
+        "Contact" => "isi-php/contact.php",
+        "Service" => "isi-php/service.php"
+    ];
